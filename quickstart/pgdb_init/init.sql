c7460fde9557a241a4eba314419cf8c207e2ffe5
dvasdekis <dimitri@vasdekis.com.au>
2019-07-24 10:33:41 +1000

    Test init with fixed parameters

--- quickstart/pgdb_init/init.sql
+++ quickstart/pgdb_init/init.sql
@@ -1,3 +1,131 @@
+-- This code is from the `pgjwt` extension:
+-- https://github.com/michelp/pgjwt
+
+CREATE OR REPLACE FUNCTION url_encode(data bytea) RETURNS text LANGUAGE sql AS $$
+    SELECT translate(encode(data, 'base64'), E'+/=\n', '-_');
+$$;
+
+
+CREATE OR REPLACE FUNCTION url_decode(data text) RETURNS bytea LANGUAGE sql AS $$
+WITH t AS (SELECT translate(data, '-_', '+/') AS trans),
+     rem AS (SELECT length(t.trans) % 4 AS remainder FROM t) -- compute padding size
+    SELECT decode(
+        t.trans ||
+        CASE WHEN rem.remainder > 0
+           THEN repeat('=', (4 - rem.remainder))
+           ELSE '' END,
+    'base64') FROM t, rem;
+$$;
+
+
+CREATE OR REPLACE FUNCTION algorithm_sign(signables text, secret text, algorithm text)
+RETURNS text LANGUAGE sql AS $$
+WITH
+  alg AS (
+    SELECT CASE
+      WHEN algorithm = 'HS256' THEN 'sha256'
+      WHEN algorithm = 'HS384' THEN 'sha384'
+      WHEN algorithm = 'HS512' THEN 'sha512'
+      ELSE '' END AS id)  -- hmac throws error
+SELECT url_encode(hmac(signables, secret, alg.id)) FROM alg;
+$$;
+
+
+CREATE OR REPLACE FUNCTION sign(payload json, secret text, algorithm text DEFAULT 'HS256')
+RETURNS text LANGUAGE sql AS $$
+WITH
+  header AS (
+    SELECT url_encode(convert_to('{"alg":"' || algorithm || '","typ":"JWT"}', 'utf8')) AS data
+    ),
+  payload AS (
+    SELECT url_encode(convert_to(payload::text, 'utf8')) AS data
+    ),
+  signables AS (
+    SELECT header.data || '.' || payload.data AS data FROM header, payload
+    )
+SELECT
+    signables.data || '.' ||
+    algorithm_sign(signables.data, secret, algorithm) FROM signables;
+$$;
+
+
+CREATE OR REPLACE FUNCTION verify(token text, secret text, algorithm text DEFAULT 'HS256')
+RETURNS table(header json, payload json, valid boolean) LANGUAGE sql AS $$
+  SELECT
+    convert_from(url_decode(r[1]), 'utf8')::json AS header,
+    convert_from(url_decode(r[2]), 'utf8')::json AS payload,
+    r[3] = algorithm_sign(r[1] || '.' || r[2], secret, algorithm) AS valid
+  FROM regexp_split_to_array(token, '\.') r;
+$$;
+alter database postgres set hasura.jwt_secret_key to 'FKlynHJWnKBJaFrB2PgWYX4xvEDY0edcy_HIWCw1AGUpeooHvUg9DfOBSzoeLh2P';
+drop function if exists hasura_auth(varchar, varchar);
+drop table if exists hasura_user;
+create table hasura_user(
+    user_id serial primary key,
+    email varchar unique,
+    crypt_password varchar,
+    cleartext_password varchar,
+    default_role varchar default 'user',
+    allowed_roles jsonb default '["user"]',
+    enabled boolean default true,
+    jwt_token text
+);
+
+create or replace function hasura_encrypt_password(cleartext_password in text, salt in text) returns varchar as $$
+    select crypt(
+        encode(hmac(cleartext_password, current_setting('hasura.jwt_secret_key'), 'sha256'), 'escape'),
+        salt);
+$$ language sql stable;
+
+create or replace function hasura_user_encrypt_password() returns trigger as $$
+begin
+    if new.cleartext_password is not null and trim(new.cleartext_password) <> '' then
+        new.crypt_password := (hasura_encrypt_password(new.cleartext_password, gen_salt('bf')));
+    end if;
+    new.cleartext_password = null;
+    new.jwt_token = null;
+    return new;
+end;
+$$ language 'plpgsql';
+
+create trigger hasura_user_encrypt_password_trigger
+before insert or update on hasura_user
+for each row execute procedure hasura_user_encrypt_password();
+
+-- https://docs.hasura.io/1.0/graphql/manual/auth/authentication/jwt.html#configuring-jwt-mode
+create or replace function hasura_auth(email in varchar, cleartext_password in varchar) returns setof hasura_user as $$
+    select
+        user_id,
+        email,
+        crypt_password,
+        cleartext_password,
+        default_role,
+        allowed_roles,
+        enabled,
+        sign(
+            json_build_object(
+                'sub', user_id::text,
+                'iss', 'Hasura-JWT-Auth',
+                'iat', round(extract(epoch from now())),
+                'exp', round(extract(epoch from now() + interval '24 hour')),
+                'https://hasura.io/jwt/claims', json_build_object(
+                    'x-hasura-user-id', user_id::text,
+                    'x-hasura-default-role', default_role,
+                    'x-hasura-allowed-roles', allowed_roles
+                )
+            ), current_setting('hasura.jwt_secret_key')) as jwt_token
+    from hasura_user h
+    where h.email = hasura_auth.email
+    and h.enabled
+    and h.crypt_password = hasura_encrypt_password(hasura_auth.cleartext_password, h.crypt_password);
+$$ language 'sql' stable;
+
+--  insert an example user and print a jwt_token
+insert into hasura_user(email, cleartext_password) values ('user@example.com', 'password');
+
+do $$ begin
+    raise notice 'Example jwt_token: %s', (select jwt_token from hasura_auth('user@example.com', 'password'));
+end $$;
 CREATE TABLE todos
 (
     id serial PRIMARY KEY,
